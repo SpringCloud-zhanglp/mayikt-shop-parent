7e1ef005dbc682de98db2bc26244981d4ab5e4a6
zhanglipei <zlp-lyq0524@163.com>
2020-03-24 19:22:12 +0800

    整合微信项目。没有加数据库查询，因为报错，也没有增加天气预报接口。

---
+++ nt-shop-service-impl/nt-shop-service-weixin/src/main/java/com/zlp/api/weixin/mp/handler/MsgHandler.java
@@ -0,0 +1,61 @@
+package com.zlp.api.weixin.mp.handler;
+
+import com.zlp.api.weixin.mp.builder.TextBuilder;
+import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.common.session.WxSessionManager;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import static me.chanjar.weixin.common.api.WxConsts.XmlMsgType;
+
+/**
+ * @author Binary Wang(https://github.com/binarywang)
+ */
+@Component
+public class MsgHandler extends AbstractHandler {
+
+    @Value("${wechat.defalutMsg}")
+    private String defalutMsg;
+
+    @Override
+    public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage,
+                                    Map<String, Object> context, WxMpService weixinService,
+                                    WxSessionManager sessionManager) {
+
+        if (!wxMessage.getMsgType().equals(XmlMsgType.EVENT)) {
+            //TODO 可以选择将消息保存到本地
+        }
+
+        //当用户输入关键词如“你好”，“客服”等，并且有客服在线时，把消息转发给在线客服
+        try {
+            if (StringUtils.startsWithAny(wxMessage.getContent(), "你好", "客服")
+                    && weixinService.getKefuService().kfOnlineList()
+                    .getKfOnlineList().size() > 0) {
+                return WxMpXmlOutMessage.TRANSFER_CUSTOMER_SERVICE()
+                        .fromUser(wxMessage.getToUser())
+                        .toUser(wxMessage.getFromUser()).build();
+            }
+        } catch (WxErrorException e) {
+            e.printStackTrace();
+        }
+
+        //TODO 组装回复消息
+//        String content = "收到信息内容：" + JsonUtils.toJson(wxMessage);
+        String content = wxMessage.getContent();
+        if (content.equals("蚂蚁课堂")) {
+            return new TextBuilder().build("牛逼6666", wxMessage, weixinService);
+        }
+        if (content.equals("张立佩")) {
+            return new TextBuilder().build("男，汉族，中国国籍，1988年8月8日出生，主要从事互联网Java架构师培训学员。", wxMessage, weixinService);
+        }
+        return new TextBuilder().build(defalutMsg, wxMessage, weixinService);
+
+    }
+
+}
