747a50e4ea7e202bc1adaea3caedbf20e985bffb
zhanglipei <zlp-lyq0524@163.com>
2020-03-27 09:34:11 +0800

    添加天气查询接口

--- nt-shop-service-impl/nt-shop-service-weixin/src/main/java/com/zlp/api/weixin/mp/handler/MsgHandler.java
+++ nt-shop-service-impl/nt-shop-service-weixin/src/main/java/com/zlp/api/weixin/mp/handler/MsgHandler.java
@@ -1,8 +1,11 @@
 package com.zlp.api.weixin.mp.handler;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.zlp.api.weixin.impl.entity.WechatKeyword;
 import com.zlp.api.weixin.impl.mapper.KeywordMapper;
 import com.zlp.api.weixin.mp.builder.TextBuilder;
+import com.zlp.http.HttpClientUtils;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpService;
@@ -26,8 +29,11 @@ public class MsgHandler extends AbstractHandler {
     @Value("${wechat.defalutMsg}")
     private String defalutMsg;
 
+    @Value("${wechat.rpcWeatherUrl}")
+    private String rpcWeatherUrl;
     @Autowired
     private KeywordMapper keywordMapper;
+
     @Override
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage,
                                     Map<String, Object> context, WxMpService weixinService,
@@ -54,7 +60,7 @@ public class MsgHandler extends AbstractHandler {
 //        String content = "收到信息内容：" + JsonUtils.toJson(wxMessage);
         String content = wxMessage.getContent();
         WechatKeyword byKeyword = keywordMapper.findByKeyword(content);
-        if (byKeyword!=null) {
+        if (byKeyword != null) {
             String keywordValue = byKeyword.getKeywordValue();
             return new TextBuilder().build(keywordValue, wxMessage, weixinService);
         }
@@ -64,6 +70,29 @@ public class MsgHandler extends AbstractHandler {
         if (content.equals("神兽")) {
             return new TextBuilder().build("男，汉族，中国国籍，1988年8月8日出生，主要从事互联网Java架构师培训学员。", wxMessage, weixinService);
         }
+
+        String newReplaceUrl = rpcWeatherUrl.replace("####", content);
+        JSONObject resultJsonObject = HttpClientUtils.httpGet(newReplaceUrl);
+        /*if (resultJsonObject != null) {
+
+            return new TextBuilder().build(resultJsonObject.toJSONString(), wxMessage, weixinService);
+        }*/
+        if (resultJsonObject != null) {
+            JSONArray results = resultJsonObject.getJSONArray("results");
+            JSONObject resultsZeroJSONObject = results.getJSONObject(0);
+            JSONObject locationJSONObject = resultsZeroJSONObject.getJSONObject("location");
+            // 地址
+            String path = locationJSONObject.getString("path");
+            JSONObject nowJSONObject = resultsZeroJSONObject.getJSONObject("now");
+
+            String text = nowJSONObject.getString("text");
+            String temperature = nowJSONObject.getString("temperature");
+            String lastUpdate = resultsZeroJSONObject.getString("last_update");
+            String resultMsg = "您当前查询的城市" + content + ",天气为" + text + "天、实时温度为:" + temperature + "℃，" +
+                    "最后更新的时间为:" + lastUpdate;
+            return new TextBuilder().build(resultMsg, wxMessage, weixinService);
+        }
+
         return new TextBuilder().build(defalutMsg, wxMessage, weixinService);
 
     }
